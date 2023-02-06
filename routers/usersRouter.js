ccbfd26f4a22b792431e8c581bd46ca8ccf06973
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-02-06 21:04:17 +0600

    file upload implementation

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,13 +1,18 @@
 // external imports
 const express = require("express");
+const { check } = require("express-validator");
 
 // internal imports
 const { getUsers } = require("../controller/userController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
 
 const router = express.Router();
 
-// login page
+// Users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+// add user
+router.post("/", avatarUpload);
 
 module.exports = router;
